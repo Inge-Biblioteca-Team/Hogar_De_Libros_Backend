abec790fff48b94d80b593a0fd66083a6013340d
Akion <akion.cheng.jimenez@est.una.ac.cr>
2024-08-25 16:37:04 -0600

    Task crear controlador, servicio y modulo de Equipo de Cómputo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { ComputersModule } from './computers/computers.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, ComputersModule],
   controllers: [AppController],
   providers: [AppService],
 })
