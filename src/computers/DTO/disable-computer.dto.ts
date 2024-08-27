479b33fd758f97e098dc0683b45077bb8d1fe7f1
Akion <akion.cheng.jimenez@est.una.ac.cr>
2024-08-27 11:36:06 -0600

    Módulo de cómputo testeado

---
+++ src/computers/DTO/disable-computer.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean } from "class-validator";
+
+export class DisableComputerDTO {
+  @IsBoolean()
+  @ApiProperty()
+  Status: boolean;
+}
