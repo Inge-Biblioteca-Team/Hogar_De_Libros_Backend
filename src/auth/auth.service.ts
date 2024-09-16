589c3fd6e1ddeefd7ee3868b5fdf20c7a3f533f6
Paula <maria.fernandez.marchena@est.una.ac.cr>
2024-09-16 17:37:12 -0600

    Add user authorization and policy for loans

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/user.entity';
+import { MailService } from './email.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UserService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private readonly mailService: MailService
   ) {}
 
   async signIn(
@@ -20,9 +24,49 @@ export class AuthService {
     if (!isMatch) {
       throw new UnauthorizedException();
     }
-    const payload = { sub: user.cedula, email: user.email };
+    const payload = { sub: user.cedula, email: user.email, role: user.role };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
   }
+
+  
+  async login(email: string, password: string): Promise<{ token: string }> {
+    const user = await this.usersService.findOne(email);
+
+    if (!user) {
+      throw new HttpException('Correo no registrado.', HttpStatus.BAD_REQUEST);
+    }
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches) {
+      throw new HttpException('Contraseña incorrecta.', HttpStatus.BAD_REQUEST);
+    }
+
+    const token = this.jwtService.sign({ id: user.cedula, role: user.role });
+    return { token };
+  }
+
+
+  async sendPasswordReset(email: string): Promise<void> {
+    const user = await this.usersService.findOne(email);
+
+    if (!user) {
+      throw new HttpException('Correo no registrado.', HttpStatus.BAD_REQUEST);
+    }
+
+    const token = this.jwtService.sign({ id: user.cedula }, { expiresIn: '1h' });
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${token}`;
+   
+
+    await this.mailService.sendPasswordReset(
+      user.email, 
+      `Haz clic en el siguiente enlace para restablecer tu contraseña: ${resetLink}` ,
+      'Restablecer tu contraseña', 
+    );
+  }
+
+
+   
+
 }
--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,14 @@ async function bootstrap() {
     .setTitle('Books API')
     .setDescription('API for managing books')
     .setVersion('1.0')
+    .addBearerAuth(  
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT', 
+      },
+      'access-token', 
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
