9dbbbac34c442ece8c8590d7a545a8df7447bf8c
Paula <maria.fernandez.marchena@est.una.ac.cr>
2024-09-24 21:38:23 -0600

    Enrollment post ,programns ,entity and post

--- src/programs/programs.module.ts
+++ src/programs/programs.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProgramsService } from './programs.service';
 import { ProgramsController } from './programs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Programs } from './programs.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Programs])],
   providers: [ProgramsService],
-  controllers: [ProgramsController]
+  controllers: [ProgramsController],
+  exports: [TypeOrmModule]
 })
 export class ProgramsModule {}
--- src/programs/programs.service.ts
+++ src/programs/programs.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Programs } from './programs.entity';
+import { CreateProgramDto } from './dto/create-program.dto';
 
 @Injectable()
-export class ProgramsService {}
+export class ProgramsService {
+  constructor(
+    @InjectRepository(Programs)
+    private readonly programsRepository: Repository<Programs>,
+  ) {}
+
+  async createProgramns(createProgramDto: CreateProgramDto): Promise<Programs> {
+    try {
+     
+      const program = this.programsRepository.create(createProgramDto);
+      
+      
+      return await this.programsRepository.save(program);
+    }catch (error) {
+            if (error.code === 'ER_DUP_ENTRY' || error.errno === 1062) {  
+              throw new BadRequestException('El programa ya existe.');
+            }
+            throw new BadRequestException('Error al crear el programa.');
+    }
+  }
+}
