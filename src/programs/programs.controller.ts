ee4efc6c58f79a037a375fafbfe76f803e781e05
Paula <maria.fernandez.marchena@est.una.ac.cr>
2024-09-25 14:30:50 -0600

    Programs disable,patch and get by id

--- src/programs/programs.controller.ts
+++ src/programs/programs.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException, Get, ParseIntPipe, Param, NotFoundException, Patch } from '@nestjs/common';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { CreateProgramDto } from './dto/create-program.dto';
 import { Programs } from './programs.entity';
 import { ProgramsService } from './programs.service';
+import { UpdateProgramsDto } from './DTO/update-course.dto';
 
 
 @ApiTags('Programs')
@@ -11,6 +12,21 @@ export class ProgramsController {
   constructor(
     private readonly programService: ProgramsService
 ) {}
+
+@Get(':id')
+@ApiResponse({ status: 201, description: 'Se ha encontrado el programa.', type: Programs })
+@ApiResponse({ status: 400, description: 'No se ha encontrado el programa .' })
+async getActiveProgramById(@Param('id', ParseIntPipe) id: number): Promise<Programs> {
+  try {
+    return await this.programService.getActiveProgramById(id);
+  } catch (error) {
+    if (error instanceof NotFoundException) {
+      throw new NotFoundException(error.message);
+    }
+    throw new BadRequestException('Programa inactivo');
+  }
+}
+
 
   @Post()
   @ApiResponse({ status: 201, description: 'Programa creado exitosamente.', type: Programs })
@@ -24,6 +40,40 @@ export class ProgramsController {
       } else {
         throw new BadRequestException('Error al crear el programa.');
       }
+    }
+  }
+
+  @Patch(':id')
+  @ApiResponse({ status: 201, description: 'Programa se ha actualizado exitosamente.', type: Programs })
+  @ApiResponse({ status: 400, description: 'Falta un campo .' })
+  async updateProgram(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProgramDto: UpdateProgramsDto
+  ): Promise<Programs> {
+    try {
+      return await this.programService.updatePrograms(id, updateProgramDto);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      }
+      throw new BadRequestException('Error al actualizar el programa.');
+    }
+  }
+  
+  @Patch(':id/disable')
+  @ApiResponse({ status: 201, description: 'Programa se ha deshabilitado exitosamente.', type: Programs })
+  @ApiResponse({ status: 400, description: 'No se ha podido deshabiliatr exitosamente .' })
+  async disableProgram(@Param('id', ParseIntPipe) id: number): Promise<{ message: string }> {
+    try {
+      return await this.programService.disableProgram(id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      }
+      if (error instanceof BadRequestException) {
+        throw new BadRequestException(error.message);
+      }
+      throw new BadRequestException('Error al deshabilitar el programa.');
     }
   }
 }
