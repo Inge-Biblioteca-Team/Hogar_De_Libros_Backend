c14ec61eb8cf234b084faa56f67c5fc695404ebf
Akion <akion.cheng.jimenez@est.una.ac.cr>
2024-10-15 22:46:27 -0600

    Conflictos

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,11 +46,11 @@ dotenv.config();
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',  
-      password: 'root',  
-      database: 'hogar_de_libros',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [
         Book,
         Computer,
