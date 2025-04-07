3d893da871029197722177dfa2bb715af2e0b610
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-04-07 00:18:19 -0600

    Dto se me olvido subirlo

---
+++ src/book-loan/DTO/ExtendLoan.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+
+import { IsDateString, IsNotEmpty } from 'class-validator';
+
+export class ExtendLoanDTO {
+  @ApiProperty({
+    example: '2025-04-17',
+    description: 'Nueva fecha de devolución',
+  })
+  @IsNotEmpty()
+  @IsDateString()
+  LoanExpirationDate: string;
+}
